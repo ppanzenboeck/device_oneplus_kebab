b211bed6f817e2d84f16f0c5e1a18309a5d083ea
ppanzenboeck <panz.development@gmail.com>
2026-01-19 15:04:06 +0100

    Revert "bp4a: replace stock keymaster rc with custom one to boot bp4a"
    This reverts commit 79613fd34aef69ac5137f517e6215f8b06f13d60.

--- device.mk
+++ device.mk
@@ -55,8 +55,3 @@ $(call inherit-product, device/oneplus/sm8250-common/common.mk)
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/kebab/kebab-vendor.mk)
-
-# BP4A Keymaster
-PRODUCT_COPY_FILES += \
-    device/oneplus/sm8250-common/rootdir/etc/init/android.hardware.keymaster@4.1-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.keymaster@4.1-service-qti.rc
-
